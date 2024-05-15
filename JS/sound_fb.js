43572c98d323c7685fc027322325f6bc95fe37a8
Glitch (glimmer-accessible-nigella) <none>
2024-05-15 04:23:05 +0000

    🚩✌️ Checkpoint ./.glitch-assets:39200795/8836 ./JS/sound_fb.js:39200795/273 ./JS/controller_fb.js:39200795/78

--- JS/sound_fb.js
+++ JS/sound_fb.js
@@ -13,11 +13,11 @@ $(function () {
             const data = snapshot.val();
 
             if(data.sd_intro == 1){
-              con.PlaySound('',1)
+              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20m%E1%BB%9F%20%C4%91%E1%BA%A7u.mp3?v=1715746744008',1)
               update(ref(db), { sd_intro : 0 })
             }
             if(data.sd_round == 1){
-              con.PlaySound('',1)
+              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20b%E1%BA%AFt%20%C4%91%E1%BA%A7u%20v%C3%B2ng%20thi.mp3?v=1715746745718',1)
               update(ref(db), { sd_round : 0 })
             }
 
