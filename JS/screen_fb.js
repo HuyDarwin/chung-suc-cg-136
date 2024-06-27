6b352ece3e7b9146d55b97f4ff321d0ca30fdf9a
Glitch (glimmer-accessible-nigella) <none>
2024-06-27 16:58:15 +0000

    💠🔑 Checkpoint ./JS/screen_fb.js:39200795/997 ./controller-25cs.html:39200795/331

--- JS/screen_fb.js
+++ JS/screen_fb.js
@@ -129,7 +129,7 @@ $(function () {
             if(data.sd_play_vid == 1) {
               $('#video-holder').css('opacity',1)
               $('#video-1').trigger('play');
-                $('#video-1')[0].playbackRate = 5;
+                $('#video-1')[0].playbackRate = 1;
               $('#video-1').on('ended',function(){
                 $('#video-holder').css('opacity',0)
                 $('#video-1').trigger('pause');
@@ -138,7 +138,7 @@ $(function () {
               update(ref(db), { sd_play_vid : 0 })
             }
           
-            if(data.sd_play_vid == 1) {
+            if(data.sd_play_vid_2 == 1) {
               $('#video-holder').css('opacity',1)
               $('#video-1').trigger('play');
                 $('#video-1')[0].playbackRate = 5;
@@ -147,7 +147,7 @@ $(function () {
                 $('#video-1').trigger('pause');
                 $('#video-1')[0].currentTime = 0;
               });
-              update(ref(db), { sd_play_vid : 0 })
+              update(ref(db), { sd_play_vid_2 : 0 })
             }
         });
       
