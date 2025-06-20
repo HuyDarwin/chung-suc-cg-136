287ad66f0fb0700537244f2d4c95b47064f32943
HuyDarwin <95059688+HuyDarwin@users.noreply.github.com>
2025-06-20 14:16:55 +0700

    Assets + Code

--- JS/sound_fb.js
+++ JS/sound_fb.js
@@ -13,111 +13,111 @@ $(function () {
             const data = snapshot.val();
 
             if(data.sd_short_intro == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/441260484_5846150375508658_2529419483320996984_n.mp3?v=1715757335615',1)
+              con.PlaySound("Assets/441260484_5846150375508658_2529419483320996984_n.mp3", 1)
               update(ref(db), { sd_short_intro : 0 })
             }
             if(data.sd_intro == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20m%E1%BB%9F%20%C4%91%E1%BA%A7u.mp3?v=1715746744008',4)
+              con.PlaySound("Assets/nhạc%20mở%20đầu.mp3", 4)
               update(ref(db), { sd_intro : 0 })
             }
             if(data.sd_credits == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20k%E1%BA%BFt%20th%C3%BAc.mp3?v=1715746741708',4)
+              con.PlaySound("Assets/nhạc%20kết%20thúc.mp3", 4)
               update(ref(db), { sd_credits : 0 })
             }
             if(data.sd_comm_in == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20v%C3%B4%20gi%E1%BA%A3i%20lao.mp3?v=1715746744890',1)
+              con.PlaySound("Assets/nhạc%20vô%20giải%20lao.mp3", 1)
               update(ref(db), { sd_comm_in : 0 })
             }
             if(data.sd_comm_out == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20h%C3%ACnh%20hi%E1%BB%87u%20k%E1%BA%BFt%20th%C3%BAc%20gi%E1%BA%A3i%20lao.mp3?v=1715746740323',1)
+              con.PlaySound("Assets/nhạc%20hình%20hiệu%20kết%20thúc%20giải%20lao.mp3", 1)
               update(ref(db), { sd_comm_out : 0 })
             }
             if(data.sd_round == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20b%E1%BA%AFt%20%C4%91%E1%BA%A7u%20v%C3%B2ng%20thi.mp3?v=1715746745718',1)
+              con.PlaySound("Assets/nhạc%20bắt%20đầu%20vòng%20thi.mp3", 1)
               update(ref(db), { sd_round : 0 })
             }
             if(data.sd_round_win == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20%C4%91%E1%BB%99i%20th%E1%BA%AFng%20v%C3%B2ng.mp3?v=1715746732004',1)
+              con.PlaySound("Assets/nhạc%20đội%20thắng%20vòng.mp3", 1)
               update(ref(db), { sd_round_win : 0 })
             }
             if(data.sd_2_players_chosen == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20ch%E1%BB%8Dn%202%20ng%C6%B0%E1%BB%9Di%20v%C3%A0o%20v%C4%91b.mp3?v=1715746729926',1)
+              con.PlaySound("Assets/nhạc%20chọn%202%20người%20vào%20vđb.mp3", 1)
               update(ref(db), { sd_2_players_chosen : 0 })
             }
           
             if(data.sd_buzzer == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20b%E1%BA%A5m%20chu%C3%B4ng_main.mp3?v=1715746747917',2)
+              con.PlaySound("Assets/nhạc%20bấm%20chuông_main.mp3", 2)
               update(ref(db), { sd_buzzer : 0 })
             }
             if(data.sd_buzzer_alt == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20b%E1%BA%A5m%20chu%C3%B4ng_alt.mp3?v=1715746747111',2)
+              con.PlaySound("Assets/nhạc%20bấm%20chuông_alt.mp3", 2)
               update(ref(db), { sd_buzzer_alt : 0 })
             }
           
             if(data.sd_reveal_iterator_4 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20hi%E1%BB%87n%20s%E1%BB%91%20%C4%91%C3%A1p%20%C3%A1n%20v%C3%B2ng%20(4%20%C4%91%C3%A1p%20%C3%A1n).mp3?v=1715746733735',3)
+              con.PlaySound("Assets/nhạc%20hiện%20số%20đáp%20án%20vòng%20(4%20đáp%20án).mp3", 3)
               update(ref(db), { sd_reveal_iterator_4 : 0 })
             }
             if(data.sd_reveal_iterator_5 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20hi%E1%BB%87n%20s%E1%BB%91%20%C4%91%C3%A1p%20%C3%A1n%20v%C3%B2ng%20(5%20%C4%91%C3%A1p%20%C3%A1n).mp3?v=1715746734553',3)
+              con.PlaySound("Assets/nhạc%20hiện%20số%20đáp%20án%20vòng%20(5%20đáp%20án).mp3", 3)
               update(ref(db), { sd_reveal_iterator_5 : 0 })
             }
             if(data.sd_reveal_iterator_6 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20hi%E1%BB%87n%20s%E1%BB%91%20%C4%91%C3%A1p%20%C3%A1n%20v%C3%B2ng%20(6%20%C4%91%C3%A1p%20%C3%A1n).mp3?v=1715746735310',3)
+              con.PlaySound("Assets/nhạc%20hiện%20số%20đáp%20án%20vòng%20(6%20đáp%20án).mp3", 3)
               update(ref(db), { sd_reveal_iterator_6 : 0 })
             }
             if(data.sd_reveal_iterator_7 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20hi%E1%BB%87n%20s%E1%BB%91%20%C4%91%C3%A1p%20%C3%A1n%20v%C3%B2ng%20(7%20%C4%91%C3%A1p%20%C3%A1n).mp3?v=1715746737103',3)
+              con.PlaySound("Assets/nhạc%20hiện%20số%20đáp%20án%20vòng%20(7%20đáp%20án).mp3", 3)
               update(ref(db), { sd_reveal_iterator_7 : 0 })
             }
             if(data.sd_reveal_iterator_8 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20hi%E1%BB%87n%20s%E1%BB%91%20%C4%91%C3%A1p%20%C3%A1n%20v%C3%B2ng%20(8%20%C4%91%C3%A1p%20%C3%A1n).mp3?v=1715746738107',3)
+              con.PlaySound("Assets/nhạc%20hiện%20số%20đáp%20án%20vòng%20(8%20đáp%20án).mp3", 3)
               update(ref(db), { sd_reveal_iterator_8 : 0 })
             }
             if(data.sd_reveal_ans == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20m%E1%BB%9F%20%C4%91%C3%A1p%20%C3%A1n%20%C4%91%C3%BAng.mp3?v=1715746742588',3)
+              con.PlaySound("Assets/nhạc%20mở%20đáp%20án%20đúng.mp3", 3)
               update(ref(db), { sd_reveal_ans : 0 })
             }
             if(data.sd_wrong == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20%C4%91%C3%A1p%20%C3%A1n%20sai.mp3?v=1715746730723',3)
+              con.PlaySound("Assets/nhạc%20đáp%20án%20sai.mp3", 3)
               update(ref(db), { sd_wrong : 0 })
             }
             if(data.sd_reveal_br_ans == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/nh%E1%BA%A1c%20hi%E1%BB%87n%20%C4%91%C3%A1p%20%C3%A1n%20v%C4%91b.mp3?v=1715746732911',3)
+              con.PlaySound("Assets/nhạc%20hiện%20đáp%20án%20vđb.mp3", 3)
               update(ref(db), { sd_reveal_br_ans : 0 })
             }
             if(data.sd_reveal_br_ans_score == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/Family_Feud_2010_file_FF201020.mp3?v=1715873757004',3)
+              con.PlaySound("Assets/Family_Feud_2010_file_FF201020.mp3", 3)
               update(ref(db), { sd_reveal_br_ans_score : 0 })
             }
             if(data.sd_br_wrong == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/Family_Feud_2010_file_FF201021.mp3?v=1715770711647',3)
+              con.PlaySound("Assets/Family_Feud_2010_file_FF201021.mp3", 3)
               update(ref(db), { sd_br_wrong : 0 })
             }
             if(data.sd_br_finished == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/Family_Feud_2010_file_FF201023.mp3?v=1715770707935',3)
+              con.PlaySound("Assets/Family_Feud_2010_file_FF201023.mp3", 3)
               update(ref(db), { sd_br_finished : 0 })
             }     
           
             if(data.sd_timer_20s == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/Family_Feud_2010_file_FF201010.mp3?v=1715770709335',5)
+              con.PlaySound("Assets/Family_Feud_2010_file_FF201010.mp3", 5)
               update(ref(db), { sd_timer_20s : 0 })
             }
             if(data.sd_timer_25s == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/Family_Feud_2010_file_FF201011.mp3?v=1715770710979',5)
+              con.PlaySound("Assets/Family_Feud_2010_file_FF201011.mp3", 5)
               update(ref(db), { sd_timer_25s : 0 })
             }
           
             if(data.sd_music_1 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/y2mate.com%20-%20Cute%20Dog%20%20Cat%20Song.mp3?v=1722592830502',1)
+              con.PlaySound("Assets/y2mate.com%20-%20Cute%20Dog%20%20Cat%20Song.mp3", 1)
               update(ref(db), { sd_music_1 : 0 })
             }
             if(data.sd_music_2 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/utomp3.com%20-%20miaw%20miaw%20miaw%20miaw%20sound%20sad%20viral%20tiktok.mp3?v=1722592838803',1)
+              con.PlaySound("Assets/utomp3.com%20-%20miaw%20miaw%20miaw%20miaw%20sound%20sad%20viral%20tiktok.mp3", 1)
               update(ref(db), { sd_music_2 : 0 })
             }
             if(data.sd_music_3 == 1){
-              con.PlaySound('https://cdn.glitch.global/d26ba743-6712-4bf4-b28e-013addcf9d55/tuy%E1%BB%87t%20%C4%91%E1%BB%89nh%20song%20ca.mp3?v=1722593158648',1)
+              con.PlaySound("Assets/tuyệt%20đỉnh%20song%20ca.mp3", 1)
               update(ref(db), { sd_music_3 : 0 })
             }
 
